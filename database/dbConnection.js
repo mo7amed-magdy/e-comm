4ee872656446b7af42afa9238c91c685bfa336fe
mo7amed-magdy <136624437+mo7amed-magdy@users.noreply.github.com>
2024-08-20 19:02:11 +0300

    third commit

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const dbConnection = ()=> {
-mongoose.connect("mongodb://127.0.0.1:27017/e-commerce").then(()=>{
+mongoose.connect("mongodb+srv://e-commUser:Mego2002@cluster0.55oeadx.mongodb.net/e-commerce").then(()=>{
     console.log("Database connected successfully");
 }).catch((err)=>{
 console.log("database error",err);
